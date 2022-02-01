1e5f1479b46a025b47894c2779ae43fa10e203b2
Bryce Schuman <bryceschuman@outlook.com>
2022-02-01 00:30:54 -0500

    Init Commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+//start the title track tile shower when the page is ready
+document.addEventListener("DOMContentLoaded", function(){
+	getMetaData();
+})
+
+//function for getting, parsing, and displaying the track info
+function getMetaData() {
+	//fetch the server info
+	fetch("https://radio.ngrok.io/status-json.xsl").then( res => {
+		//parse the json from the server
+		res.json().then( json => {
+			//display the track title
+			document.querySelector("#trackTitle").innerHTML = "NOW PLAYING: " + json.icestats.source.title;
+		})
+	})
+	setTimeout(getMetaData,5000);
+};
+function getArtwork () {
+	
+}
+
